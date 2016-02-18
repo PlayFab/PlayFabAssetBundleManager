ea710fdec50383e3eea22fc34ed9de7181bc6384
MarcoWilliams <marco@playfab.com>
2016-02-17 18:08:48 -0800

    PlayFabAssetBundleManager Tool and .UnityPackage

--- AssetBundleManagerClient/Assets/_Example/LoadAssetBundles.cs
+++ AssetBundleManagerClient/Assets/_Example/LoadAssetBundles.cs
@@ -39,20 +39,7 @@ public class LoadAssetBundles : MonoBehaviour
                 ThruCDN = true
             }, (contentDownloadResult) =>
             {
-                /*
-                Debug.Log(contentDownloadResult.URL);
-                var urlParams = contentDownloadResult.URL.Split('?');
-                var baseUrl = urlParams[0].Replace(platformKey + manifestFileName.ToLower(), "");
-                Debug.Log(baseUrl);
-                AssetBundleManager.SimulateAssetBundleInEditor = false;
-                AssetBundleManager.SetSourceAssetBundleURL(baseUrl);
-                var request = AssetBundleManager.Initialize(manifestFileName.ToLower(), urlParams[1]);
-                if (request != null)
-                {
-                    StartCoroutine(request);
-                }
-                */
-
+                //Here we are kicking off a coroutine to download the inital manifest bundle.
                 StartCoroutine(LoadBundles(contentDownloadResult.URL, platformKey, manifestFileName));
 
             },PlayFabErrorHandler.HandlePlayFabError);
@@ -67,11 +54,20 @@ public class LoadAssetBundles : MonoBehaviour
     {
 
         Debug.Log(contentUrl);
+
+        //Get the url params ( in this case it is the AWS Security on the file ).
         var urlParams = contentUrl.Split('?');
+        
+        //Get the base URL for the bundle.
         var baseUrl = urlParams[0].Replace(platformKey + manifestFileName.ToLower(), "");
         Debug.Log(baseUrl);
+
+        //Make sure you turn this off, or it will go into simulation mode and not use playfab CDN
         AssetBundleManager.SimulateAssetBundleInEditor = false;
+        //Set the source download URL for the bundle.
         AssetBundleManager.SetSourceAssetBundleURL(baseUrl);
+
+        //Initialize the AssetBundleManager with the Manifest bundle
         var request = AssetBundleManager.Initialize(manifestFileName.ToLower(), urlParams[1]);
         if (request != null)
         {
@@ -80,6 +76,9 @@ public class LoadAssetBundles : MonoBehaviour
 
         var bundleKey = string.Format("{0}/{1}", BundleRoot, platformKey);
         yield return StartCoroutine(InstantiateGameObjectAsync(bundleKey + "weapons", "weapons" , "dagger"));
+
+        //If you had more bundles to load, then you can do it here with more yield return coroutines.
+
     }
 
     protected IEnumerator InstantiateGameObjectAsync(string assetBundleName, string bundleName, string assetName)
@@ -87,20 +86,26 @@ public class LoadAssetBundles : MonoBehaviour
         // This is simply to get the elapsed time for this phase of AssetLoading.
         float startTime = Time.realtimeSinceStartup;
 
-
         var isDone = false;
         var urlParams = string.Empty;
-
+        
+        //We have to make a call to playfab to get the URL for each asset bundle that we want to download.
         PlayFabClientAPI.GetContentDownloadUrl(new GetContentDownloadUrlRequest()
         {
             Key = assetBundleName,
             HttpMethod = "GET"
         }, (getContentResult) =>
         {
+            //Get the URL Params (AWS Security)
             urlParams = getContentResult.URL.Split('?')[1];
+            //Flag that we are done calling playfab
             isDone = true;
         } ,PlayFabErrorHandler.HandlePlayFabError);
 
+
+        //This is a little hoakey, because PlayFab API's use a callback system, which doesn't quite Jive with 
+        //Coroutines,  so we are waiting .5f seconds, before we proceed which gives PlayFab Ample time to respond.
+        //You can probably reduce this delay to .2f and it would still work.
         if (!isDone)
         {
             yield return new WaitForSeconds(.5f);
